bb3ffea80ee9fcec249f3d3cc96d80b0ce5f321c
Elizabeth Murray <lizmurray479@gmail.com>
2016-10-13 20:14:18 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  console.log('test')
+  res.render('index', { title: 'Liz - Home' })
+})
+
+router.get('/contact', function (req, res, next) {
+  console.log('called contact render')
+  res.render('contact', { title: 'Contact Me!' })
+})
+
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About Me!' })
+})
+
+module.exports = router
